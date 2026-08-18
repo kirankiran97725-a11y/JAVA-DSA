5204785f42e98838f03496db355edfc71391bc6b
Kiran <kirankiran97725@gmail.com>
2026-08-18 10:13:15 +0530

    Updated Java DSA code

---
+++ src/day2/NESTEDcondition.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class NESTEDcondition {
+    public static void main(String[] args)
+    {
+        int marks=45;
+        if(marks>=45)
+        {
+            if(marks<=95)
+            {
+                System.out.println("SECURED DISTINTION");
+            }
+        }
+    }
+}
