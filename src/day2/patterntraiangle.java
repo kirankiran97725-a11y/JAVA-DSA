826b922cc4918a8a6b95455168df6a6fdb67f039
Kiran <kirankiran97725@gmail.com>
2026-08-18 12:15:15 +0530

    Updated Java DSA code

--- src/day2/patterntraiangle.java
+++ src/day2/patterntraiangle.java
@@ -1,14 +1,15 @@
 package day2;
 
 public class patterntraiangle {
-
-        public static void main(String[] args) {
-            for (int i = 1; i <= 5; i++) {
-                for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
-
+}
