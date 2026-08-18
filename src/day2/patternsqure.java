486c8bd6eb93d194e52522b3836ade0b6c309543
Kiran <kirankiran97725@gmail.com>
2026-08-18 12:13:30 +0530

    Updated Java DSA code

--- src/day2/patternsqure.java
+++ src/day2/patternsqure.java
@@ -5,9 +5,9 @@ public class patternsqure {
     {
         for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=5;j++)
+            for(int j=1;j<=10;j++)
             {
-                System.out.print("*");
+                System.out.print("/");
 
 
             }
