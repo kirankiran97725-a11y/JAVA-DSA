9ec2899474bda52dd19561458297609d70cb921c
Kiran <kirankiran97725@gmail.com>
2026-08-19 09:44:31 +0530

    Updated Java DSA code

---
+++ src/day3/student.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class student {
+    public static void main(String[] args){
+
+            student s=new student();
+            System.out.println("student object"+s);
+        }
+    }
+
