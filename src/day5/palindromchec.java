4cf126ae38a82c041b3bab8e5ad09ecd15d29959
Kiran <kirankiran97725@gmail.com>
2026-08-27 15:12:06 +0530

    Updated Java DSA code

---
+++ src/day5/palindromchec.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class Main {
+    public static void main(String[] args)
+        String str="madam";
+    int left=0;
+    int right=str,length()-1;
+    boolean ispalindrome=true;
+while(left<right){
+    }
+
+}
