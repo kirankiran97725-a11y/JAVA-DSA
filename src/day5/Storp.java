f422693faf728926e3cc9d5fec1ad86819f1d82e
Kiran <kirankiran97725@gmail.com>
2026-08-27 09:41:53 +0530

    Updated Java DSA code

---
+++ src/day5/Storp.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class String {
+        public static void main(String[]args)
+        {
+            String str="Hello";
+
+            System.out.println("the string=="+str);
+            System.out.println("String length=="+str.length());
+            System.out.println("the first character=="+str.charAt(0));
+            System.out.println("the second character=="+str.charAt(1));
+        }
+    }
+
+}
